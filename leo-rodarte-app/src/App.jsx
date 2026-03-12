e6297303270f401eda7b6e950255de8df38e6536
pppi21 <leorodarte07@gmail.com>
2026-03-12 16:19:06 +0000

    5.5 done

--- leo-rodarte-app/src/App.jsx
+++ leo-rodarte-app/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import Message from './Message';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <h1>Leo Rodarte App</h1>
-      <Message text="Welcome to my React project!" name="Leo Rodarte" />
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increase Count</button>
+      <Message text={`Button has been clicked ${count} times!`} name="Leo Rodarte" />
       <Message text="Props make components reusable." name="Leo" />
     </div>
   );
